01265f3ce60003c70a91f5889376c1b55a461f2d
Jhon Vasquez <jhonv.v1@gmail.com>
2022-09-22 12:06:28 -0500

    configuracion mongoose y db connect

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
+require ("dotenv").config()
 const express= require("express");
+const {connect} = require("./db");
+
 
 const app = express();
 const port = 8080;
+connect();
 
 app.listen(port, ()=>{
     console.log('Servidor Ok')
