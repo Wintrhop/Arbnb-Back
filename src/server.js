353a649edb3e8e532c190bdc50d4b098b133d2e1
Jhon Vasquez <jhonv.v1@gmail.com>
2022-09-23 09:21:39 -0500

    configuracion morgan, cors y port

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 require ("dotenv").config()
 const express= require("express");
+const cors = require("cors")
+const morgan = require("morgan")
 const {connect} = require("./db");
 
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
+
+app.use(cors())
+app.use(morgan("tiny"))
+app.use(express.json())
+
 connect();
 
 app.listen(port, ()=>{
