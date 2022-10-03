41927f4fce44908178826cc0e7f7a275b1c3ab8d
Diego <drhiguerag@unal.edu.co>
2022-10-03 15:52:48 -0500

    auth.js

---
+++ src/api/Reservations/reservation.controller.js
@@ -0,0 +1,13 @@
+const Reservation = require("./reservation.model")
+
+module.exports = {
+    async create (req,res) {
+        try{
+            const data = req.body
+
+            
+        } catch(err){
+
+        }
+    }
+}
