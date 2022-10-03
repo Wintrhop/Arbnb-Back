3d2d3aa7bc13069f9bb93bcb7a9452f5f3b956db
Ruben Dario Castilla Romero <67343176+CastRome@users.noreply.github.com>
2022-10-03 18:32:01 -0500

    singin and singup

--- src/api/Users/Users.route.js
+++ src/api/Users/Users.route.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userController = require('./Users.controller');
 
-router.route('/').get(userController.list);
-router.route('/:userId').get(userController.show);
-router.route('/').post(userController.create);
+//router.route('/').get(userController.list);
+//router.route('/:userId').get(userController.show);
+//router.route('/').post(userController.create);
 router.route('/:userId').put(userController.update);
 router.route('/:userId').delete(userController.destroy);
+router.route('/singup').post(userController.singup);
+router.route('/singin').post(userController.singin);
 
 module.exports = router;
