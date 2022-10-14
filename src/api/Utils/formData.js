63ca52de520a0641c60ed5b1aae70d3781f6d4ae
Diego <drhiguerag@unal.edu.co>
2022-10-14 07:23:56 -0500

    busboy and cloudinary

---
+++ src/api/Utils/formData.js
@@ -0,0 +1,8 @@
+const busboy = require("busboy");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY_CLOUD,
+  api_secret: process.env.CLUD_SECRET,
+});
